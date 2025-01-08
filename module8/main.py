13b42af7f6c288edb9cc777cb944722c84bf18b0
Ayyadurai <kmayyadurai286@gmail.com>
2025-01-08 21:04:53 +0530

    Add module 8 for age verification and voting eligibility check

---
+++ module8/main.py
@@ -0,0 +1,10 @@
+name = input("Enter name : ")
+age = int(input("Enter Age : "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote")
+    
+
+print("End of program")
