d0f1c3cdfaa5eaa8f74f76697f934ad55e4101e8
Ayyadurai <kmayyadurai286@gmail.com>
2025-01-09 20:53:59 +0530

    module 8 completed

---
+++ module8/elif.py
@@ -0,0 +1,12 @@
+number = int(input("Enter Number : "))
+
+if number > 10 :
+    print("Number is greater than 10")
+elif number < 10 : 
+    print("Number is less than 10")
+else :
+    print("Number is equal to  10")
+    
+# https://www.geeksforgeeks.org/nested-if-statement-in-python/
+
+
