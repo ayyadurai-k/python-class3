a580e50d52e56f20e0f43595c59502fc23dc5735
Ayyadurai <kmayyadurai286@gmail.com>
2025-01-13 20:38:12 +0530

    dictionary loop completed

---
+++ module9/dictloop.py
@@ -0,0 +1,25 @@
+
+
+paint_details = {
+    "name" : "Asian Wall Paints",
+    "colour" : "Shimmering Pink",
+    "price" : 400
+}
+
+
+print(" Keys : ")
+
+
+for key in list(paint_details.keys()) :
+    print(key)
+
+print("Values : ")
+
+for value in list(paint_details.values()):
+    print(value)
+    
+print(" Items : ")
+
+
+for item in list(paint_details.items()):
+    print(item)
