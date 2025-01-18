cd732b5dee005e8b14d65044099c4300f0598b50
Ayyadurai <kmayyadurai286@gmail.com>
2025-01-18 21:05:41 +0530

    Implemented the functions

---
+++ module11/main.py
@@ -0,0 +1,14 @@
+
+def sum_numbers():
+    a=10
+    b=5
+    result=a+b
+    print("Result : ",result)
+
+sum_numbers()
+sum_numbers()
+sum_numbers()
+sum_numbers()
+sum_numbers()
+sum_numbers()
+sum_numbers()
---
+++ module11/param.py
@@ -0,0 +1,7 @@
+
+def sum_numbers(a,b):
+    result=a+b
+    print("Result : ",result)
+
+sum_numbers(10,5)
+sum_numbers(-2,-6)
