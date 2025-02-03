f8549fae5e015d0c0e6fd55d5d01c3687610dfe0
Ayyadurai <kmayyadurai286@gmail.com>
2025-02-03 20:39:26 +0530

    Super finished

---
+++ module17/main.py
@@ -0,0 +1,25 @@
+class People:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_people_details(self):
+        print("name : ", self.name)
+        print("age : ", self.age)
+
+
+class Student(People):
+    def __init__(self, name, age, reg_no, mark):
+        self.reg_no = reg_no
+        self.mark = mark
+        super().__init__(name, age)
+
+    def display_student_details(self):
+        print("regno : ", self.reg_no)
+        print("mark : ", self.mark)
+
+
+student1 = Student("Jebisha", 19, 100, 97)
+
+student1.display_people_details()
+student1.display_student_details()
